9b26923c0afc38c44d456aa0f1b071d5f166a1b6
Maxim Kutprot <maksim.kutprot@daubit.org>
2023-09-13 16:45:52 +0200

    react 2

---
+++ react/2.jsx
@@ -0,0 +1,11 @@
+const JSX = (
+  <div>
+    <h1>Paragraph One</h1>
+    <p>Paragraph Two</p>
+    <ul>
+      <li>Paragraph One</li>
+      <li>Paragraph One</li>
+      <li>Paragraph One</li>
+    </ul>
+  </div>
+);
